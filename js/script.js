0b14082ca516039c284538c09da5353d918da823
Brian Marinai <brianmarinai.bm@gmail.com>
2020-07-07 16:32:19 +0200

    progetto con jumbo terminato

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 $(document).ready(function (){
-  $(".parent-dropdown").click(function (){
+  $(".parent-dropdown").mouseover(function (){
     $(this).children(".dropdown").toggle();
   });
 });
+
+
+
+// $(document).ready(function (){
+//   $(".parent-dropdown").mouseover(function (){
+//     var active = $(".dropdown").hasClass("hidden");
+//     if (active == false) {
+//       $(".dropdown").addCLass("hidden");
+//     } else {
+//       $(".dropdown").removeCLass("hidden");
+//
+//     }
+//     // $(this).children(".dropdown").toggle();
+//   });
+// });
