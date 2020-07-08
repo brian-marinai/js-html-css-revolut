d808f427e9a9e3a7e420ed661c3ca151f5649046
Brian Marinai <brianmarinai.bm@gmail.com>
2020-07-08 15:54:26 +0200

    commit con parte di burger-menu

--- js/script.js
+++ js/script.js
@@ -2,8 +2,20 @@ $(document).ready(function (){
   $(".parent-dropdown").hover(function (){
     $(this).children(".dropdown").toggleClass("hidden active");
   });
+  $(".icon").click(
+  function() {
+    var active = $(".hamburger-menu").hasClass("active");
+    if (active == false) {
+      $(".hamburger-menu").addClass("active");
+    } else {
+      $(".hamburger-menu").removeClass("active");
+    }
+  }
+);
+  $(".parent-dropdown-burger").hover(function (){
+    $(this).children(".dropdown").toggleClass("hidden active");
+  });
 });
-
 
 
 // $(document).ready(function (){
